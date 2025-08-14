0da8eea3e716b7b9cdbe8ae417c35151d0b280b9
Kaznacheev Pavel <kaznacheev9@yandex.ru>
2025-08-14 12:48:55 +0300

    refactor: move magic numbers and strings to the configuration

--- src/main/java/ru/kaznacheev/notification/config/ApplicationProperties.java
+++ src/main/java/ru/kaznacheev/notification/config/ApplicationProperties.java
@@ -1,6 +1,7 @@
 package ru.kaznacheev.notification.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -8,21 +9,64 @@ import org.springframework.context.annotation.Configuration;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
+@Getter
 public class ApplicationProperties {
 
+    private final SseProperties sseProperties;
+    private final EventsProperties eventsProperties;
+    private final RedisRetryProperties redisRetryProperties;
+    private final RedisTopicsProperties redisTopicsProperties;
+
     @Configuration
-    @ConfigurationProperties(prefix = "app.sse")
+    @ConfigurationProperties(prefix = "application.sse")
     @Getter
     @Setter
     public static class SseProperties {
-
         private Duration heartbeatInterval;
         private Duration subscriberRemoveDelay;
-        private int redisMaxRetries;
-        private Duration redisRetryBackoff;
-        private Duration redisMaxBackoff;
-        private int maxBufferSize;
-        private String redisChannelPrefix;
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix = "application.sse.events")
+    @Getter
+    @Setter
+    public static class EventsProperties {
+        private Event heartbeat;
+        private Event start;
+        private Event error;
+        private Event notification;
+
+        @Getter
+        @Setter
+        public static class Event {
+            private String eventName;
+            private String comment;
+        }
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix = "application.redis.retry")
+    @Getter
+    @Setter
+    public static class RedisRetryProperties {
+        private int maxAttempts;
+        private Duration initialBackoff;
+        private Duration maxBackoff;
+        private double jitterFactor;
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix = "application.redis.topics")
+    @Getter
+    @Setter
+    public static class RedisTopicsProperties {
+        private String userIdPlaceholder;
+        private String userNotificationPattern;
+
+        public String buildNotificationTopic(String userId) {
+            return userNotificationPattern.replace(userIdPlaceholder, userId);
+        }
 
     }
 
--- src/main/java/ru/kaznacheev/notification/service/impl/EventServiceImpl.java
+++ src/main/java/ru/kaznacheev/notification/service/impl/EventServiceImpl.java
@@ -24,24 +24,24 @@ public class EventServiceImpl implements EventService {
 
     private final ReactiveRedisMessageListenerContainer messageListenerContainer;
     private final Map<String, Subscriber> sinks = new ConcurrentHashMap<>();
-    private final ApplicationProperties.SseProperties properties;
+    private final ApplicationProperties properties;
 
     @Override
     public Flux<ServerSentEvent<Object>> createNotificationsStream(String userId) {
         Subscriber subscriber = sinks.computeIfAbsent(userId, this::createSubscriber);
         subscriber.getSubscribersCount().incrementAndGet();
 
-        Flux<ServerSentEvent<Object>> heartbeatStream = Flux.interval(properties.getHeartbeatInterval())
+        Flux<ServerSentEvent<Object>> heartbeatStream = Flux.interval(properties.getSseProperties().getHeartbeatInterval())
                 .map(tick -> ServerSentEvent.builder()
-                        .event("PING")
-                        .comment("heartbeat")
+                        .event(properties.getEventsProperties().getHeartbeat().getEventName())
+                        .comment(properties.getEventsProperties().getHeartbeat().getComment())
                         .build());
 
         return subscriber.getSink().asFlux()
                 .mergeWith(heartbeatStream)
                 .startWith(ServerSentEvent.builder()
-                        .event("CONNECT")
-                        .comment("connect")
+                        .event(properties.getEventsProperties().getStart().getEventName())
+                        .comment(properties.getEventsProperties().getStart().getComment())
                         .build())
                 .doFinally(signalType -> {
                     if (subscriber.getSubscribersCount().decrementAndGet() == 0) {
@@ -51,7 +51,7 @@ public class EventServiceImpl implements EventService {
     }
 
     private void removeSubscriber(String userId, Subscriber subscriber) {
-        Mono.delay(properties.getSubscriberRemoveDelay())
+        Mono.delay(properties.getSseProperties().getSubscriberRemoveDelay())
                 .subscribe(aLong -> {
                     if (subscriber.getSubscribersCount().get() == 0 && sinks.remove(userId, subscriber)) {
                         subscriber.getRedisDisposable().dispose();
@@ -61,27 +61,28 @@ public class EventServiceImpl implements EventService {
     }
 
     private Subscriber createSubscriber(String userId) {
-        ChannelTopic topic = new ChannelTopic(properties.getRedisChannelPrefix() + userId);
+        ChannelTopic topic = new ChannelTopic(properties.getRedisTopicsProperties().buildNotificationTopic(userId));
         Sinks.Many<ServerSentEvent<Object>> sink = Sinks.many().multicast().onBackpressureBuffer();
 
         Disposable redisSubscriber = messageListenerContainer.receive(topic)
                 .map(ReactiveSubscription.Message::getMessage)
                 .doOnNext(message -> {
                     sink.tryEmitNext(ServerSentEvent.builder()
-                            .event("EVENT")
+                            .event(properties.getEventsProperties().getNotification().getEventName())
                             .data(message)
+                            .comment(properties.getEventsProperties().getNotification().getComment())
                             .build());
                 })
                 .doOnError(error -> {
                     sink.tryEmitNext(ServerSentEvent.builder()
-                                    .event("ERROR")
-                                    .comment("Internal error")
+                                    .event(properties.getEventsProperties().getError().getEventName())
+                                    .comment(properties.getEventsProperties().getError().getComment())
                             .build());
                 })
                 .retryWhen(
-                        Retry.backoff(properties.getRedisMaxRetries(), properties.getRedisRetryBackoff())
-                                .maxBackoff(properties.getRedisMaxBackoff())
-                                .jitter(0.5))
+                        Retry.backoff(properties.getRedisRetryProperties().getMaxAttempts(), properties.getRedisRetryProperties().getInitialBackoff())
+                                .maxBackoff(properties.getRedisRetryProperties().getMaxBackoff())
+                                .jitter(properties.getRedisRetryProperties().getJitterFactor()))
                 .subscribe();
         return new Subscriber(sink, redisSubscriber);
     }
